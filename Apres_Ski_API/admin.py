c7a23dadfcd9ae9a0745351ceb285c2f536de15b
Joseph Hilby <joseph.hilby@gmail.com>
2023-02-07 16:50:44 -0600

    Register classes with admin site

--- Apres_Ski_API/admin.py
+++ Apres_Ski_API/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from models/user import User
+from models/restaurant import Restaurant
+from models/hour import Hour
+from models/happyhour import HappyHour
+from models/photo import Photo
+from models/engagement import Engagement
+from models/comment import Comment
+from models/lift import Lift
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Restaurant)
+admin.site.register(Hour)
+admin.site.register(HappyHour)
+admin.site.register(Photo)
+admin.site.register(Engagement)
+admin.site.register(Comment)
+admin.site.register(Lift)
