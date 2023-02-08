a52441610c2f5a004da580e1d7b9a9271deef0de
Joseph Hilby <joseph.hilby@gmail.com>
2023-02-08 11:35:36 -0600

    Add engagement to GET /restaurant endpoint

--- Apres_Ski_API/serializers/restaurant.py
+++ Apres_Ski_API/serializers/restaurant.py
@@ -11,10 +11,10 @@ from .engagement import EngagementSerializer
 class RestaurantSerializer(serializers.ModelSerializer):
   hour = HourSerializer(many=True, read_only=True)
   happyhour = HappyHourSerializer(many=True, read_only=True)
+  engagement = EngagementSerializer(many=True, read_only=True)
   # photo = PhotoSerializer(many=True, read_only=True)
-  # engagement = EngagementSerializer(many=True, read_only=True)
   # comment = CommentSerializer(many=True, read_only=True)
 
   class Meta:
     model = Restaurant
-    fields = ["id", 'name', 'address', 'food_type', 'cost', 'vibe', 'cover_img', 'alt_text', 'lat', 'lon', 'alcoholic_drinks', 'has_happy_hour', 'hour', 'happyhour']
+    fields = ["id", 'name', 'address', 'food_type', 'cost', 'cover_img', 'alt_text', 'lat', 'lon', 'alcoholic_drinks', 'has_happy_hour', 'hour', 'happyhour', 'engagement']
