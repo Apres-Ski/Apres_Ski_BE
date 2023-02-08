ba5ad20bbd41e29094a401b5328047b99191c608
Joseph Hilby <joseph.hilby@gmail.com>
2023-02-08 13:58:14 -0600

    Update user model for new seed data from FE

--- Apres_Ski_API/models/user.py
+++ Apres_Ski_API/models/user.py
@@ -5,3 +5,5 @@ class User(models.Model):
     app_label = 'Apres_Ski_API'
 
   name = models.CharField(max_length=60)
+  lat = models.CharField(max_length=20)
+  lon = models.CharField(max_length=20)
