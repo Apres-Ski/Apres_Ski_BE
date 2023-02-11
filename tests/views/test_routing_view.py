f5dfb6fd4fe8932207ba196647880ca63ad453f8
Joseph Hilby <joseph.hilby@gmail.com>
2023-02-11 16:05:50 -0600

    Update routing test

--- tests/views/test_routing_view.py
+++ tests/views/test_routing_view.py
@@ -1,10 +1,6 @@
 import pytest
 import json
-from django.test import RequestFactory, TestCase, Client
-
-# from tests.factories import UserFactory
-# from Apres_Ski_API.models.user import User
-from Apres_Ski_API.views.routing import RoutingViewSet
+from django.test import TestCase, Client
 
 class UserViewTests(TestCase):
   def test_routing_get(self):
@@ -16,9 +12,27 @@ class UserViewTests(TestCase):
     }
     client = Client()
     response = client.get("/api/v1/routing/", data)
-    import ipdb; ipdb.set_trace()
-    # view = UserViewSet.as_view({'get': 'retrieve'})
-    # response = view(request)
-    # content = json.loads(response.content)
-    # assert response.status_code == 200
-    # assert response.data['id'] == user.id
+    content = json.loads(response.content)
+
+    assert response.status_code == 200
+    assert isinstance(content, dict)
+    assert len(content) == 3
+    features = content['features']
+    assert isinstance(features, list)
+    assert len(features) == 1
+    feature = features[0]
+    assert len(feature) == 3
+    assert feature['type'] == 'Feature'
+    assert isinstance(feature['properties'], dict)
+    assert len(feature['properties']) == 7
+    assert feature['properties']['mode'] == 'walk'
+    assert isinstance(feature['properties']['waypoints'], list)
+    assert len(feature['properties']['waypoints']) == 2
+    assert feature['properties']['units'] == 'metric'
+    assert feature['properties']['distance'] == 1078
+    assert feature['properties']['distance_units'] == 'meters'
+    assert feature['properties']['time'] == 1128.991
+    assert isinstance(feature['properties']['legs'], list)
+    assert len(feature['properties']['legs']) == 1
+    assert isinstance(feature['properties']['legs'][0], dict)
+    assert isinstance(feature['geometry'], dict)
