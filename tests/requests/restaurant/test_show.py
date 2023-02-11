40ae1968e575a053eaa91148defc402f62aa7151
Joseph Hilby <joseph.hilby@gmail.com>
2023-02-11 09:50:21 -0600

    Fix typo add sad path test

--- tests/requests/restaurant/test_show.py
+++ tests/requests/restaurant/test_show.py
@@ -4,7 +4,7 @@ from django.test import TestCase, Client
 from tests.factories import RestaurantFactory
 
 class RestaurantShowTests(TestCase):
-    def test_restaurant_list(self):
+    def test_restaurant_show(self):
         restaurant = RestaurantFactory()
         client = Client()
         response = client.get(f"/api/v1/restaurant/{restaurant.id}/")
@@ -35,3 +35,22 @@ class RestaurantShowTests(TestCase):
         assert isinstance(entry['attributes']['hour'], list)
         assert isinstance(entry['attributes']['happyhour'], list)
         assert isinstance(entry['attributes']['engagement'], list)
+
+    def test_restaurant_show_404(self):
+        restaurant = RestaurantFactory()
+        client = Client()
+        response = client.get(f"/api/v1/restaurant/{restaurant.id + 1}/")
+        assert response.status_code == 404
+
+        content = json.loads(response.content)
+        assert isinstance(content, dict)
+        assert len(content) == 1
+        assert isinstance(content['errors'], list)
+        assert len(content['errors']) == 1
+
+        error = content['errors'][0]
+        assert isinstance(error, dict)
+        assert len(error) == 3
+        assert error['detail'] == 'Not found.'
+        assert error['status'] == '404'
+        assert error['code'] == 'not_found'
