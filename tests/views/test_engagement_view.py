ddb80124ae305a804ff972a04d1836aa30593d97
Kevin Ta <kvnta@yahoo.com>
2023-02-11 14:51:51 -0700

    Passing all engagement tests for views

--- tests/views/test_engagement_view.py
+++ tests/views/test_engagement_view.py
@@ -2,7 +2,8 @@ import json
 import pytest
 from django.test import RequestFactory, TestCase
 
-from tests.factories import EngagementFactory
+from tests.factories import EngagementFactory, RestaurantFactory, UserFactory
+from Apres_Ski_API.models.engagement import Engagement
 from Apres_Ski_API.views.engagement import EngagementViewSet
 
 class EngagementViewTests(TestCase):
@@ -12,7 +13,7 @@ class EngagementViewTests(TestCase):
     view = EngagementViewSet.as_view({'get': 'retrieve'})
     response = view(request, pk=engagement.pk)
     assert response.status_code == 200
-    assert response.data['id'] == engagementx.difference(y)
+    assert response.data['id'] == engagement.id
 
   def test_engagement_get_all(self):
     EngagementFactory.create_batch(4)
@@ -20,10 +21,13 @@ class EngagementViewTests(TestCase):
     view = EngagementViewSet.as_view({'get': 'list'})
     response = view(request)
     assert response.status_code == 200
-    assert len(response.data) == 3
+    assert len(response.data) == 4
 
   def test_engagement_post(self):
-    engagement = { "vibe": "Lively", "rating": "4.5", "favorites": True}
+    restaurant_1 = RestaurantFactory()
+    user_1 = UserFactory()
+    engagement = {"vibe": "Lively", "rating": "4.5", "favorites": True,
+                  "restaurant": f"{restaurant_1.pk}", "user": f"{user_1.pk }"}
     request = RequestFactory().post("api/v1/engagement/", engagement)
     view = EngagementViewSet.as_view({'post': 'create'})
     assert not Engagement.objects.exists()
@@ -31,15 +35,33 @@ class EngagementViewTests(TestCase):
     response = view(request, data)
     assert response.status_code == 201
     assert  Engagement.objects.count() == 1
-    assert Engagement.objects.get(pk = 1).vibe == 'casual'
+    assert Engagement.objects.get(pk = 1).vibe == 'Lively'
 
   def test_engagement_delete(self): 
     engagement = EngagementFactory()
     assert Engagement.objects.count() == 1
     request = RequestFactory().delete(f"api/v1/engagement/{engagement.pk}")
     view = EngagementViewSet.as_view({'delete': 'destroy'})
-    response = view(request, pk=engagment.pk)
+    response = view(request, pk=engagement.pk)
     assert not Engagement.objects.exists()
     assert response.status_code == 204
 
-  
+  def test_engagement_patch(self):
+    engagement = EngagementFactory(vibe='Lively')
+    engagement_update = { "data": { "type": "Engagement", "id": f"{engagement.pk}", "attributes": {"vibe": "Family Friendly"}}}
+    data = json.dumps(engagement_update)
+    request = RequestFactory().patch(f"api/v1/engagement/{engagement.pk}", data, content_type='application/vnd.api+json') 
+    view = EngagementViewSet.as_view({'patch': 'partial_update'})
+    response = view(request, pk=engagement.pk)
+    updated_engagement = Engagement.objects.get(pk = engagement.pk)
+    assert updated_engagement.vibe == 'Family Friendly'
+    assert response.status_code == 200
+
+  def test_engagement_404(self):
+    engagement = EngagementFactory()
+    request = RequestFactory().get(f"api/v1/engagement/{engagement.pk + 1}")
+    view = EngagementViewSet.as_view({'get': 'retrieve'})
+    response = view(request, pk=engagement.pk + 1)
+    assert response.status_code == 404
+
+                                                                   
