9141a237d270f112c870ed74634d914eb7e4f3f5
Kevin Ta <kvnta@yahoo.com>
2023-02-11 10:35:54 -0700

    Finish restaurant model test

--- tests/models/test_restaurant.py
+++ tests/models/test_restaurant.py
@@ -4,15 +4,30 @@ from Apres_Ski_API.models.restaurant import restaurant
 from tests.factories import RestaurantFactory
 
 class RestaurantTests(TestCase): 
-    def setUp(self): 
-        self.restaurant = RestaurantFactory(name="name",
-                                            address='address',
-                                            food_type='food_type',
-                                            cost='cost',
-                                            vibe='vibe',
-                                            cover_img='cover_img',
-                                            alt_text='alt_text',
-                                            lat='lat',
-                                            lon='lon',
-                                            alcoholic_drinks='alcoholic_drinks',
-                                            has_happy_hour='has_happy_hour')
+  def setUp(self): 
+      self.restaurant = RestaurantFactory(name="name",
+                                          address='address',
+                                          food_type='food_type',
+                                          cost='cost',
+                                          vibe='vibe',
+                                          cover_img='cover_img',
+                                          alt_text='alt_text',
+                                          lat='lat',
+                                          lon='lon',
+                                          alcoholic_drinks='alcoholic_drinks',
+                                          has_happy_hour='has_happy_hour')
+  
+  def test_restaurant_instance(self):
+     assert isinstance(self.restaurant, Restaurant)
+  
+  def test_restaurant_state(self):
+     assert self.restaurant.name == 'name'
+     assert self.restaurant.address == 'address'
+     assert self.restaurant.food_type == 'food_type'
+     assert self.restaurant.vibe == 'vibe'
+     assert self.restaurant.cover_img == 'cover_img'
+     assert self.restaurant.alt_text == 'alt_text'
+     assert self.restaurant.lat == 'lat'
+     assert self.restaurant.lon == 'lon'
+     assert self.restaurant.alcoholic_drinks == 'alcoholic_drinks'
+     assert self.restaurant.has_happy_hour == 'has_happy_hour'
