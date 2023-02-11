ed67fbb5e8dabc644765bc94b53615831badb8d6
Joseph Hilby <joseph.hilby@gmail.com>
2023-02-11 09:50:59 -0600

    Remove old comments

--- tests/views/test_restaurant_view.py
+++ tests/views/test_restaurant_view.py
@@ -5,12 +5,10 @@ from django.test import RequestFactory, TestCase
 from tests.factories import RestaurantFactory
 from Apres_Ski_API.views.restaurant import RestaurantViewSet
 
-# @pytest.mark.django_db
 class RestaurantViewTests(TestCase):
   def test_restaurant_200_OK(self):
     request = RequestFactory().get("api/v1/restaurant")
     view = RestaurantViewSet.as_view({'get': 'retrieve'})
     restaurant = RestaurantFactory()
     response = view(request, pk=restaurant.pk)
-    # import ipdb; ipdb.set_trace()
     assert response.status_code == status.HTTP_200_OK
