fdecf6d1ab0e65e43a5ae33d4717e22d9c62315f
Kevin Ta <kvnta@yahoo.com>
2023-02-11 14:12:10 -0700

    Engagement view test index, show, post, delete

--- tests/views/test_engagement_view.py
+++ tests/views/test_engagement_view.py
@@ -1,3 +1,4 @@
+import json
 import pytest
 from django.test import RequestFactory, TestCase
 
@@ -6,8 +7,39 @@ from Apres_Ski_API.views.engagement import EngagementViewSet
 
 class EngagementViewTests(TestCase):
   def test_engagement_200_OK(self):
-    request = RequestFactory().get("api/v1/engagement")
-    view = EngagementViewSet.as_view({'get': 'retrieve'})
     engagement = EngagementFactory()
+    request = RequestFactory().get(f"api/v1/engagement/{engagement.pk}")
+    view = EngagementViewSet.as_view({'get': 'retrieve'})
     response = view(request, pk=engagement.pk)
     assert response.status_code == 200
+    assert response.data['id'] == engagementx.difference(y)
+
+  def test_engagement_get_all(self):
+    EngagementFactory.create_batch(4)
+    request = RequestFactory().get(f"api/v1/engagement/")
+    view = EngagementViewSet.as_view({'get': 'list'})
+    response = view(request)
+    assert response.status_code == 200
+    assert len(response.data) == 3
+
+  def test_engagement_post(self):
+    engagement = { "vibe": "Lively", "rating": "4.5", "favorites": True}
+    request = RequestFactory().post("api/v1/engagement/", engagement)
+    view = EngagementViewSet.as_view({'post': 'create'})
+    assert not Engagement.objects.exists()
+    data = json.dumps(engagement)
+    response = view(request, data)
+    assert response.status_code == 201
+    assert  Engagement.objects.count() == 1
+    assert Engagement.objects.get(pk = 1).vibe == 'casual'
+
+  def test_engagement_delete(self): 
+    engagement = EngagementFactory()
+    assert Engagement.objects.count() == 1
+    request = RequestFactory().delete(f"api/v1/engagement/{engagement.pk}")
+    view = EngagementViewSet.as_view({'delete': 'destroy'})
+    response = view(request, pk=engagment.pk)
+    assert not Engagement.objects.exists()
+    assert response.status_code == 204
+
+  
