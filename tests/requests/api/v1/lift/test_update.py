71cfca6fa3e3e72390e7aa237b67a2755af8818f
Krisetn Nestler <kristen.nestler@gmail.com>
2023-02-12 01:44:10 -0500

    lift_test_update

---
+++ tests/requests/api/v1/lift/test_update.py
@@ -0,0 +1,58 @@
+import pytest
+import json
+from django.test import TestCase, Client
+from tests.factories import LiftFactory
+
+class LiftUpdateTests(TestCase):
+    def test_lift_update(self):
+        lift = LiftFactory()
+        client = Client()
+        lift_update = {
+                      "data": {
+                          "type": "Lift",
+                          "id": f"{lift.pk}",
+                          "attributes": {
+                            "name": "new_name"
+                          }
+                      }
+        }
+        data = json.dumps(lift_update)
+        response = client.patch(f"/api/v1/lift/{lift.pk}/", data, content_type='application/vnd.api+json')
+
+        assert response.status_code == 200
+        content = json.loads(response.content)
+        assert isinstance(content, dict)
+        assert len(content) == 1
+        assert isinstance(content['data'], dict)
+        assert len(content['data']) == 3
+
+        entry = content['data']
+        assert isinstance(entry['type'], str)
+        assert entry['type'] == 'Lift'
+        assert isinstance(entry['id'], str)
+        assert isinstance(entry['attributes'], dict)
+        assert len(entry['attributes']) == 3
+        assert isinstance(entry['attributes']['name'], str)
+        assert entry['attributes']['name'] == 'new_name'
+        assert isinstance(entry['attributes']['lat'], str)
+        assert isinstance(entry['attributes']['lon'], str)
+ 
+
+    def test_lift_update_404(self):
+        lift = LiftFactory()
+        client = Client()
+        response = client.patch(f"/api/v1/lift/{lift.id + 1}/")
+        assert response.status_code == 404
+
+        content = json.loads(response.content)
+        assert isinstance(content, dict)
+        assert len(content) == 1
+        assert isinstance(content['errors'], list)
+        assert len(content['errors']) == 1
+
+        error = content['errors'][0]
+        assert isinstance(error, dict)
+        assert len(error) == 3
+        assert error['detail'] == 'Not found.'
+        assert error['status'] == '404'
+        assert error['code'] == 'not_found'
