d6460e49a7801aff55208298483a01a210d5688e
Joseph Hilby <joseph.hilby@gmail.com>
2023-02-12 06:17:57 -0600

    Uncommented code

--- Apres_Ski_API/views/routing.py
+++ Apres_Ski_API/views/routing.py
@@ -1,12 +1,12 @@
-# import requests
-# from django.http import JsonResponse
-# from rest_framework import viewsets
-# from keys import routing_key
+import requests
+from django.http import JsonResponse
+from rest_framework import viewsets
+from keys import routing_key
 
-# class RoutingViewSet(viewsets.ViewSet):
-#   def retrieve(self, request):
-#     params = request.GET
-#     response = requests.get(f"https://api.geoapify.com/v1/routing?waypoints={params['user_lat']},{params['user_lon']}|{params['rest_lat']},{params['rest_lon']}&mode=walk&apiKey={routing_key}")
+class RoutingViewSet(viewsets.ViewSet):
+  def retrieve(self, request):
+    params = request.GET
+    response = requests.get(f"https://api.geoapify.com/v1/routing?waypoints={params['user_lat']},{params['user_lon']}|{params['rest_lat']},{params['rest_lon']}&mode=walk&apiKey={routing_key}")
 
-#     data = response.json()
-#     return JsonResponse(data)
+    data = response.json()
+    return JsonResponse(data)
