1b565ee45477aec811b12ed5dc9b81cc1f0525c6
Joseph Hilby <joseph.hilby@gmail.com>
2023-02-13 13:13:24 -0600

    Remove db setting to troubleshoot ci issues

--- Apres_Ski_BE/settings.py
+++ Apres_Ski_BE/settings.py
@@ -92,11 +92,6 @@ DATABASES = {
 }
 }
 
-DATABASES['default'] = dj_database_url.config(
-    conn_max_age=600,
-    conn_health_checks=True,
-)
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
